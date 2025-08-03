8eff3981d47531b1061b12654d142631b3d6ae0c
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-08-03 12:56:53 +0530

    add code for title and logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './App.css';
 import FamilyForm from './component/FamilyForm.js';
 import FamilyTree from './component/FamilyTree/FamilyTree.js';
-
+import Logo from './assets/image.png'
 function App() {
 
 
   return (
-    <div className="container">
-      <FamilyTree />
-      <div>
-        <h1>FamilyRoots: Family tree management system</h1>
-        <FamilyForm />
+    <>
+      <div className='header'>
+       <div className='header'>
+         <img src={Logo} alt="FamilyRoots logo" aria-label='logo' />
+        <p>FamilyRoots</p>
+       </div>
+        <span>Family Tree Management System</span>
       </div>
-    </div>
+      <div className="container">
+        <FamilyTree />
+        <div>
+          <FamilyForm />
+        </div>
+      </div>
+    </>
   );
 }
 
