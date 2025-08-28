89940538632d503c8e826a8180bb8a98e5f76ce4
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-08-28 14:55:54 +0530

    fix: adjust container styles for improved layout and update form input color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import FamilyForm from './component/FamilyForm.js';
 import FamilyTree from './component/FamilyTree/FamilyTree.js';
 import Header from './component/Header/Header.js';
+import HeroSection from './component/Hero/HeroSection.js';
 function App() {
 
   return (
     <>
       <Header/>
+      <HeroSection/>
       <div className="container">
         <FamilyTree />
         <div>
