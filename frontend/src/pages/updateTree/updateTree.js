566238ed0810fc1ee059644e89020fc4ad65fec7
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-08-28 15:06:45 +0530

    Adds button styles and removes unused updateTree component
    Introduces new button styles for enhanced UI consistency.
    
    Removes the outdated updateTree component and its associated styles to streamline the codebase and eliminate redundancy.

--- frontend/src/pages/updateTree/updateTree.js
+++ frontend/src/pages/updateTree/updateTree.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './updateTree.css'
-import Header from '../Header/Header';
+import Header from '../../component/Header/Header';
 import { toast } from 'react-toastify'
 const UpdateTree = () => {
     const [formData, setFormData] = useState({
